74a931d5deb2e9d6e7b1d8f0666aba08075fc386
ladober <lado.beridze@gmail.com>
2024-01-17 23:40:37 +0400

    done

--- script.js
+++ script.js
@@ -8,50 +8,51 @@ let pcPlayer = document.querySelector("#js-pc-pic");
 let playerCount = document.querySelector("#js-counter-player");
 let pcPlayerCount = document.querySelector("#js-counter-pc");
 
+let winner = document.querySelector("#winner");
 let pCount = 0;
 let pcCount = 0;
 
 rockBtn.addEventListener("click", () => {
-  player.src = "img/user-moves/rock-1.png";
+  player.src = "img/user-moves/rr.png";
   let randomNum = Math.floor(Math.random() * 3);
 
   if (randomNum == 0) {
-    pcPlayer.src = "img//user-moves/paper-1.png";
+    pcPlayer.src = "img//user-moves/pp.png";
     pcPlayerCount.textContent = ++pcCount;
   } else if (randomNum == 1) {
-    pcPlayer.src = "img/user-moves/rock-1.png";
+    pcPlayer.src = "img/user-moves/rr.png";
   } else if (randomNum == 2) {
-    pcPlayer.src = "img/user-moves/scissors-1.png";
+    pcPlayer.src = "img/user-moves/ss.png";
     playerCount.textContent = ++pCount;
   }
 });
 
 scissorsBtn.addEventListener("click", () => {
-  player.src = "img/user-moves/scissors-1.png";
+  player.src = "img/user-moves/ss.png";
   let randomNum = Math.floor(Math.random() * 3);
 
   if (randomNum == 0) {
-    pcPlayer.src = "img/user-moves/paper-1.png";
+    pcPlayer.src = "img/user-moves/pp.png";
     playerCount.textContent = ++pCount;
   } else if (randomNum == 1) {
-    pcPlayer.src = "img/user-moves/rock-1.png";
+    pcPlayer.src = "img/user-moves/rr.png";
     pcPlayerCount.textContent = ++pcCount;
   } else if (randomNum == 2) {
-    pcPlayer.src = "img/user-moves/scissors-1.png";
+    pcPlayer.src = "img/user-moves/ss.png";
   }
 });
 
 paperBtn.addEventListener("click", () => {
-  player.src = "img/user-moves/paper-1.png";
+  player.src = "img/user-moves/pp.png";
   let randomNum = Math.floor(Math.random() * 3);
 
   if (randomNum == 0) {
-    pcPlayer.src = "img/user-moves/paper-1.png";
+    pcPlayer.src = "img/user-moves/pp.png";
   } else if (randomNum == 1) {
-    pcPlayer.src = "img/user-moves/rock-1.png";
+    pcPlayer.src = "img/user-moves/rr.png";
     playerCount.textContent = ++pCount;
   } else if (randomNum == 2) {
-    pcPlayer.src = "img/user-moves/scissors-1.png";
+    pcPlayer.src = "img/user-moves/ss.png";
     pcPlayerCount.textContent = ++pcCount;
   }
 });
